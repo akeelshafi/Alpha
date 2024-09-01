9f625cc013aa726d6f0ab59ac350ef6c9237d578
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-01 17:08:07 +0530

    Practice Day 1

--- src/JavaBasics/FirstProgram.java
+++ src/JavaBasics/FirstProgram.java
@@ -4,9 +4,17 @@ public class FirstProgram {
     public static void main(String[] args) {
        // System.out.println("Hello java after long time");
         //System.out.println("****\n***\n**\n*");
-        System.out.println("****");
+       /* System.out.println("****");
         System.out.println("***");
         System.out.println("**");
-        System.out.println("*");
+        System.out.println("*");*/
+
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        a = 50;
+        System.out.println(a);
+
     }
 }
