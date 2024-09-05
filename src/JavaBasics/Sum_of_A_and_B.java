dfc968fdcf34f65e0e00bac6510030a8eb62e8ff
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-05 11:57:31 +0530

    Practice Day 2

---
+++ src/JavaBasics/Sum_of_A_and_B.java
@@ -0,0 +1,13 @@
+package JavaBasics;
+
+public class Sum_of_A_and_B {
+    public static void main(String[] args){
+        // sum of a and b
+
+        int a = 50;
+        int b = 60;
+
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
