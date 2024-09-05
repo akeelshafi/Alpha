1322bf7e9df68ac010396a8a6371845aa5eae44a
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-05 11:41:52 +0530

    Practice Day 2

--- src/JavaBasics/varaible.java
+++ src/JavaBasics/varaible.java
@@ -9,11 +9,16 @@ public class varaible {
         short s = 20; // Takes 2 bytes
         System.out.println(s);
 
+        char c = 'a'; // Takes 2 bytes
+        System.out.println(c);
+
         int i =  300; // Takes 4 bytes
         System.out.println(i);
 
         float f = 200.7f; // Takes 4 bytes
         System.out.println(f);
+
+
 
 
 
