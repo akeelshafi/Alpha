49df68931a41b110da1256d7a1317a52135d0abf
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-05 11:45:38 +0530

    Practice Day 2

--- src/JavaBasics/varaible.java
+++ src/JavaBasics/varaible.java
@@ -3,11 +3,15 @@ package JavaBasics;
 public class varaible {
     public static void main(String []args){
         //Data Types in Java( 8 primitive data types)
+
         byte b = 20;// Takes 1 byte
         System.out.println(b);
 
         short s = 20; // Takes 2 bytes
         System.out.println(s);
+
+        int i =  300; // Takes 4 bytes
+        System.out.println(i);
 
         char c = 'a'; // Takes 2 bytes
         System.out.println(c);
@@ -15,11 +19,14 @@ public class varaible {
         boolean ft = true; // Takes true or false
         System.out.println(ft);
 
-        int i =  300; // Takes 4 bytes
-        System.out.println(i);
-
         float f = 200.7f; // Takes 4 bytes
         System.out.println(f);
+
+        double d = 20000.5; // takes 8 byte
+        System.out.println(d);
+
+        long l =  11000000;
+        System.out.println(l);
 
 
 
