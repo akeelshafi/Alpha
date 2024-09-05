200726646d98858d6a6917cfabd65bede2e8549c
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-05 14:49:50 +0530

    Practice Day 2

---
+++ src/JavaBasics/Area_Of_Circle.java
@@ -0,0 +1,19 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Area_Of_Circle {
+    public static void main(String[] args){
+        // calculating area of circle
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of radius: ");
+        float radius = sc.nextFloat();
+
+        float pi = 3.14f;
+
+        float area = pi*radius*radius;
+        System.out.println(area);
+    }
+}
