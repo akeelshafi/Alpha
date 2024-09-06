e1b97eb1a2f3d555c694de7bbe3290d4ef88257e
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-06 06:38:12 +0530

    Practice Day 2

---
+++ src/JavaBasics/Type_Conversion.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class Type_Conversion {
+    public static void main(String[] args){
+
+        float number = 20.56789f;
+        int number2= (int)number;
+        System.out.println(number2);
+    }
+}
