4719922ffe5f2bef68fcc2f20dda05ec4956f6bc
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-07 07:35:22 +0530

    Practice Day

--- src/JavaBasics/Operators_Problems.java
+++ src/JavaBasics/Operators_Problems.java
@@ -3,14 +3,25 @@ package JavaBasics;
 public class Operators_Problems {
     public static void main(String [] args){
 
-        int x = 2, y = 5;
+     /*   int x = 2, y = 5;
 
         int exp1 = (x * y / x);
         int exp2 = (x * (y / x));
 
         System.out.print(exp1 +",");
         System.out.print(exp2);
+*/
 
+        int x = 200, y = 50, z = 100;
 
+        if(x>y && y>z){
+            System.out.println("Hello");
+        }
+        if (z>y && z<x){
+            System.out.println("Java");
+        }
+        if ((y+200)<x && (y+150) < z){
+            System.out.println("Hello Java");
+        }
     }
 }
