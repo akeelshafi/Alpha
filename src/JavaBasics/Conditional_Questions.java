b8e52be71d391bf045efaa153dd84175939bec96
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-08 15:40:09 +0530

    Practice Day 5

--- src/JavaBasics/Conditional_Questions.java
+++ src/JavaBasics/Conditional_Questions.java
@@ -6,7 +6,7 @@ public class Conditional_Questions {
     public static void main(String[] args) {
         /*Question 1 : Write a Java program to get a number from the user and print whether it is
         positive or negative*/
-        Scanner sc = new Scanner(System.in);
+       /* Scanner sc = new Scanner(System.in);
         System.out.print("Enter number: ");
         int num = sc.nextInt();
 
@@ -14,6 +14,16 @@ public class Conditional_Questions {
             System.out.println(num + " is positive");
         } else {
             System.out.println(num + " is negative");
+        }*/
+
+        /*Question 2 : Finish the following code so that it prints You have a fever if your temperature
+        is above 100 and otherwise prints You don't have a fever.*/
+
+        double temp = 103.5;
+        if (temp>100){
+            System.out.println("you have a fever");
+        }else {
+            System.out.println("You don't have fever");
         }
     }
 }
