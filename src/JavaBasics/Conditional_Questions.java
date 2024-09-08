ebe304bfd3b77f2e67838bafa7a92c52e3f368fc
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-08 16:02:44 +0530

    Practice Day 5

--- src/JavaBasics/Conditional_Questions.java
+++ src/JavaBasics/Conditional_Questions.java
@@ -19,11 +19,33 @@ public class Conditional_Questions {
         /*Question 2 : Finish the following code so that it prints You have a fever if your temperature
         is above 100 and otherwise prints You don't have a fever.*/
 
-        double temp = 103.5;
+       /* double temp = 103.5;
         if (temp>100){
             System.out.println("you have a fever");
         }else {
             System.out.println("You don't have fever");
+        }*/
+
+      /*  int a = 63, b = 36;
+        boolean x = (a < b ) ? true : false;
+        int y= (a > b ) ? a : b;*/
+
+        /*Question 5 : Write a Java program that takes a year from the user and print whether that
+        year is a leap year or not.*/
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is Leap year");
+
+        } else {
+            System.out.println(year + " is not Leap year");
         }
+
+
     }
 }
+
