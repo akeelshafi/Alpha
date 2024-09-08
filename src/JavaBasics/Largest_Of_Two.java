5334d8d6362302b29b266db9f222bcef3adafdfc
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-08 10:23:52 +0530

    Practice Day 5

---
+++ src/JavaBasics/Largest_Of_Two.java
@@ -0,0 +1,15 @@
+package JavaBasics;
+
+public class Largest_Of_Two {
+    public  static void  main(String[] args){
+
+        int a = 20;
+        int b = 10;
+
+        if (a>=b){
+            System.out.println("A is largest of 2");
+        }else {
+            System.out.println("B is largest of 2");
+        }
+    }
+}
--- src/JavaBasics/Operators_Problems.java
+++ src/JavaBasics/Operators_Problems.java
@@ -33,19 +33,16 @@ public class Operators_Problems {
         z/=(x+y);
         System.out.println(x+" "+ y+" "+ z);*/
 
-       /* int x=9, y =12;
-        int a =2, b=4,c=6;
+        int x = 9, y = 12;
+        int a = 2, b = 4, c = 6;
+        int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+        System.out.println(exp);
 
-        int exp = 4/3 * (x+34) + 9 * (a+b*c) + (3+y*(12 + a)) / (a+ b*y);
-
-        System.out.println(exp);*/
-
-
-                int x = 10, y = 5;
+               /* int x = 10, y = 5;
                 int exp1 = (y * (x / y + x / y));
                 int exp2 = (y * x / y + y * x / y);
                 System.out.println(exp1);
-                System.out.println(exp2);
+                System.out.println(exp2);*/
 
     }
 }
