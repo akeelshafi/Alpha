edc1425839b29aacb7473f5c88513585962b36c8
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-08 14:40:22 +0530

    Practice Day 5

--- src/JavaBasics/Ternary_Operator.java
+++ src/JavaBasics/Ternary_Operator.java
@@ -2,10 +2,15 @@ package JavaBasics;
 
 public class Ternary_Operator {
     public static void main(String[] args){
-        int number = 4;
+ /*       int number = 4;
 
         // ternary operator
         String type = ((number%2==0)?"even":"odd");
         System.out.println(type);
+        */
+
+        int marks = 20;
+        String result = marks>=33? "Pass":"fail";
+        System.out.println(result);
     }
 }
