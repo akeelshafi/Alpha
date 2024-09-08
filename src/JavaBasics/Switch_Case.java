4a24923cf9e0b496e942d006173f9d9c5895e258
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-08 15:08:53 +0530

    Practice Day 5

--- src/JavaBasics/Switch_Case.java
+++ src/JavaBasics/Switch_Case.java
@@ -11,29 +11,37 @@ public class Switch_Case {
         int day = sc.nextInt();
 
 
-        switch (day){
-            case 1: System.out.println("Monday");
-            break;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
 
-            case 2: System.out.println("Tuesday");
-            break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
 
-            case 3: System.out.println("Wednesday");
-            break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
 
-            case 4: System.out.println("Thursday");
-            break;
+            case 4:
+                System.out.println("Thursday");
+                break;
 
-            case 5: System.out.println("Friday");
-            break;
+            case 5:
+                System.out.println("Friday");
+                break;
 
-            case 6: System.out.println("Saturday");
-            break;
+            case 6:
+                System.out.println("Saturday");
+                break;
 
-            case 7: System.out.println("Sunday");
-            break;
+            case 7:
+                System.out.println("Sunday");
+                break;
 
-            default: System.out.println("Enjoy your day");
+            default:
+                System.out.println("Enjoy your day");
 
         }
     }
