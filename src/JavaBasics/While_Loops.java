bd339b421ad8295c887e5bfdfb18b5a9614c3b9f
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-09 06:40:29 +0530

    Practice Day 6

--- src/JavaBasics/While_Loops.java
+++ src/JavaBasics/While_Loops.java
@@ -1,14 +1,29 @@
 package JavaBasics;
 
+import java.util.Scanner;
+
 public class While_Loops {
     public static void main(String[] args) {
 
         // print numbers from 1-10
-        int number = 1;
+        /*int number = 1;
 
         while (number <= 10) {
             System.out.print(number+" ");
             number++;
+        }*/
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Range: ");
+        int range = sc.nextInt();
+
+        int counter = 1;
+
+        while (counter <= range) {
+            System.out.print(counter +" ");
+            counter++;
         }
+
     }
 }
