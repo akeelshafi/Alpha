c718a991533c0924579ef2933fb3cc3e99dbd544
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-09 06:34:18 +0530

    Practice Day 6

---
+++ src/JavaBasics/While_Loops.java
@@ -0,0 +1,14 @@
+package JavaBasics;
+
+public class While_Loops {
+    public static void main(String[] args) {
+
+        // print numbers from 1-10
+        int number = 1;
+
+        while (number <= 10) {
+            System.out.print(number+" ");
+            number++;
+        }
+    }
+}
