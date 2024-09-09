bc25d58f73f2d49c66eaabc2f198abaf33d75e4d
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-09 07:06:11 +0530

    Practice Day 6

---
+++ src/JavaBasics/Square_Pattern.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class Square_Pattern {
+    public static void main(String[] args) {
+
+        for (int line = 1; line <= 4; line++) {
+            System.out.println("****");
+        }
+    }
+}
