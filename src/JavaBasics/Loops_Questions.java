cb8ec2e5e4535d6473584d053258a5ccd75ae301
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-10 07:16:44 +0530

    Practice Day 7

---
+++ src/JavaBasics/Loops_Questions.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class Loops_Questions {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++) {
+            System.out.println("Hello");
+            i+=2;
+        }
+    }
+}
