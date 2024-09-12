2481d4e932494e7d1092dee4cbd0d995c9de2331
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-12 06:58:15 +0530

    Practice Day 9

---
+++ src/JavaBasics/Functions_Methods.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+public class Functions_Methods {
+
+    public static void printHelloWorld(){
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args){
+      printHelloWorld();
+    }
+}
