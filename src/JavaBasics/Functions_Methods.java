47bf2a75e81ba3428228ce4eaa5a939b6d8b91dd
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-12 07:00:28 +0530

    Practice Day 9

--- src/JavaBasics/Functions_Methods.java
+++ src/JavaBasics/Functions_Methods.java
@@ -6,7 +6,15 @@ public class Functions_Methods {
         System.out.println("Hello world");
     }
 
+    public static int calculateSum(int a, int b){
+
+        int sum = a +b;
+        return sum;
+    }
+
     public static void main(String[] args){
       printHelloWorld();
+      int sum =calculateSum(5,255);
+        System.out.println(sum);
     }
 }
