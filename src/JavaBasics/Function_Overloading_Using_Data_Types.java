97385c457f8eb4d76aa13386f3e1547c2dfb4339
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-13 11:29:58 +0530

    Practice Day 10

---
+++ src/JavaBasics/Function_Overloading_Using_Data_Types.java
@@ -0,0 +1,25 @@
+package JavaBasics;
+
+public class Function_Overloading_Using_Data_Types {
+
+    // calculate sum  of integer float double numbers
+
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static float sum(float a, float b){
+        return a+b;
+    }
+
+    public static double sum(double a, double b){
+        return a+b;
+    }
+    public static void main(String[] args){
+
+        System.out.println("Sum of integer value is: "+sum(10,20));
+        System.out.println("Sum of floating value is: "+ sum(10.0f,20.0f));
+        System.out.println("Sum of double value is: "+ sum(10,20));
+
+    }
+}
