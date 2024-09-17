653add00c342aee2c51ad69b5be2e4978c7264d9
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-09-17 19:50:34 +0530

    Practice Day 13

---
+++ src/JavaBasics/Is_Even.java
@@ -0,0 +1,21 @@
+package JavaBasics;
+
+public class Is_Even {
+    public static int isEven(int a){
+
+        if (a % 2 == 0) {
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
+        return  a;
+    }
+    public static void main(String[] args){
+      /*  Question 2 : Write a method named isEven that accepts an int argument. The method
+        should return true if the argument is even, or false otherwise. Also write a program to test your
+        method.*/
+
+        int isEven = isEven(10);
+
+    }
+}
